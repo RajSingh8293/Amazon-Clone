2aab4842c9613cafb6d58f5bdf8b1f89c217fc68
Rajsingh <singhraj@gmail.com>
2024-03-06 22:04:31 +0530

    Changes index files

---
+++ Js/data.js
@@ -0,0 +1,203 @@
+// const data = [
+// 	{
+// 		id: 1,
+// 		image: "https://m.media-amazon.com/images/I/71tXq7kL7kL._AC_UL320_.jpg",
+// 		description: "AUTOMET Womens 2 Piece Outfits Lounge Matching Sets Two PieceLinen Shorts Crop Tops 2023 Trendy Clothes",
+// 		price: 40,
+// 		small_price: 79,
+// 		shipping: "Shps to Japan"
+
+// 	},
+// 	{
+// 		id: 2,
+// 		image: "https://m.media-amazon.com/images/I/71iRhfKuYqL._AC_UL320_.jpg",
+// 		description: "COOFANDY Men's 2 Pieces Shirt Set Short Sleeve Button Down Casual Hippie Holiday Beach T- Shirts Shorts Outfits",
+// 		price: 37,
+// 		small_price: 79,
+// 		shipping: "Shps to Japan"
+
+// 	},
+// 	{
+// 		id: 3,
+// 		image: "https://m.media-amazon.com/images/I/51VrNZqMuTL._AC_UL320_.jpg",
+// 		description: "PRETTYGARDEN Women's Open Front Cardigan Sweaters Fashion Button Down Cable Knit Chunky Outwear Coats",
+// 		price: 43,
+// 		small_price: 79,
+// 		shipping: "Shps to Japan"
+
+// 	},
+
+// 	{
+// 		id: 4,
+// 		image: "https://m.media-amazon.com/images/I/61iTccPNO0L._AC_UL320_.jpg",
+// 		description: "Caracilia Women's Fashion Two Piece Outfits Sweater Sets Cozy Knit Tops & Casual Pants Tracksuit Lounge Sets",
+// 		price: 51,
+// 		small_price: 79,
+// 		shipping: "Shps to Japan"
+
+// 	},
+// 	{
+// 		id: 5,
+// 		image: "https://m.media-amazon.com/images/I/81O6ERy19CL._AC_UL320_.jpg",
+// 		description: "Trendy Queen Womens Oversized Sweatshirts Hoodies Half Zip Pullover Fall Fashion Outfits 2023 Y2k Clothes",
+// 		price: 48,
+// 		small_price: 79,
+// 		shipping: "Shps to Japan"
+
+// 	},
+// 	{
+// 		id: 6,
+// 		image: "https://m.media-amazon.com/images/I/71AUhL7Z6LL._AC_UL320_.jpg",
+// 		description: "Trendy Queen Womens Oversized Sweatshirts Hoodies Half Zip Pullover Fall Fashion Outfits 2023 Y2k Clothes",
+// 		price: 88,
+// 		small_price: 79,
+// 		shipping: "Shps to Japan"
+
+// 	},
+// 	{
+// 		id: 7,
+// 		image: "https://m.media-amazon.com/images/I/718ppqAzXUL._AC_UL320_.jpg",
+// 		description: "Trendy Queen Womens Oversized Sweatshirts Hoodies Half Zip Pullover Fall Fashion Outfits 2023 Y2k Clothes",
+// 		price: 98,
+// 		small_price: 79,
+// 		shipping: "Shps to Japan"
+
+// 	},
+// 	{
+// 		id: 8,
+// 		image: "https://m.media-amazon.com/images/I/51A8597saDL._AC_UL320_.jpg",
+// 		description: "TAUTOMET Womens 2 Piece Outfits Lounge Matching Sets Two Piece Linen Shorts Spring Tops 2023 Trendy Clothes",
+// 		price: 67,
+// 		small_price: 79,
+// 		shipping: "Shps to Japan"
+
+// 	},
+// 	{
+// 		id: 9,
+// 		image: "https://m.media-amazon.com/images/I/61Lic0hVJ4L._AC_UL320_.jpg",
+// 		description: "TAUTOMET Womens 2 Piece Outfits Lounge Matching Sets Two Piece Linen Shorts Spring Tops 2023 Trendy Clothes",
+// 		price: 57,
+// 		small_price: 79,
+// 		shipping: "Shps to Japan"
+
+// 	},
+// 	{
+// 		id: 10,
+// 		image: "https://m.media-amazon.com/images/I/71quJKjPKnL._AC_UL320_.jpg",
+// 		description: "TAUTOMET Womens 2 Piece Outfits Lounge Matching Sets Two Piece Linen Shorts Spring Tops 2023 Trendy Clothes",
+// 		price: 78,
+// 		small_price: 79,
+// 		shipping: "Shps to Japan"
+
+// 	},
+// 	{
+// 		id: 11,
+// 		image: "https://m.media-amazon.com/images/I/71e0cRA7T5L._AC_UL320_.jpg",
+// 		description: "TAUTOMET Womens 2 Piece Outfits Lounge Matching Sets Two Piece Linen Shorts Spring Tops 2023 Trendy Clothes",
+// 		price: 69,
+// 		small_price: 79,
+// 		shipping: "Shps to Japan"
+
+// 	},
+// 	{
+// 		id: 12,
+// 		image: "https://m.media-amazon.com/images/I/71dF6cSR3TL._MCnd_AC_UL320_.jpg",
+// 		description: "TAUTOMET Womens 2 Piece Outfits Lounge Matching Sets Two Piece Linen Shorts Spring Tops 2023 Trendy Clothes",
+// 		price: 99,
+// 		small_price: 79,
+// 		shipping: "Shps to Japan"
+
+// 	},
+// 	{
+// 		id: 13,
+// 		image: "https://m.media-amazon.com/images/I/71Sp8NHWkoL._AC_UL320_.jpg",
+// 		description: "TAUTOMET Womens 2 Piece Outfits Lounge Matching Sets Two Piece Linen Shorts Spring Tops 2023 Trendy Clothes",
+// 		price: 59,
+// 		small_price: 89,
+// 		shipping: "Shps to Japan"
+
+// 	},
+// 	{
+// 		id: 14,
+// 		image: "https://m.media-amazon.com/images/I/61yGIQ2UBoL._AC_UL320_.jpg",
+// 		description: "TAUTOMET Womens 2 Piece Outfits Lounge Matching Sets Two Piece Linen Shorts Spring Tops 2023 Trendy Clothes",
+// 		price: 49,
+// 		small_price: 99,
+// 		shipping: "Shps to Japan"
+
+// 	},
+// 	{
+// 		id: 15,
+// 		image: "https://m.media-amazon.com/images/I/81DFscJikOL._AC_UL320_.jpg",
+// 		description: "TAPAFES Women Summer Cotton Linen Short Sets 2 Piece Shorts Top Lounge Beach Vacation Two Piece",
+// 		price: 89,
+// 		small_price: 69,
+// 		shipping: "Shps to Japan"
+
+// 	},
+
+// ]
+
+const getData = async () => {
+
+
+	const fetchdata = await fetch('https://fakestoreapi.com/products')
+	const data = await fetchdata.json()
+
+	let dataValue = ''
+	data.forEach((value) => {
+		dataValue += `<div class="box">
+		<div class="box-img">
+			<img src="${value.image}"  alt>
+		</div>
+	
+		<div class="box-content">
+			<div class="box-text">
+				<a href="/Pages/productDetails.html">${value.description.slice(0, 40)}...
+	</a >
+			</div >
+			<div class="stars">
+				<span>
+					<i class="fa fa-star" aria-hidden="true"></i>
+				</span>
+				<span>
+					<i class="fa fa-star" aria-hidden="true"></i>
+				</span>
+				<span>
+					<i class="fa fa-star" aria-hidden="true"></i>
+				</span>
+				<span>
+					<i class="fa fa-star" aria-hidden="true"></i>
+				</span>
+				<span>
+					<i class="fa fa-star" aria-hidden="true"></i>
+				</span>
+				<div class="count">
+					<a class="rating-count" href>1,627</a>
+				</div>
+			</div>
+			<div class="price">
+				<a href>
+					<span>$</span>
+					<span class="price-text">${value.price}</span>
+					<span></span>
+				</a>
+			</div>
+			<div class="delivery">
+				<span>Delivery</span>
+				<span class="delivery-text">Fri, Oct 27</span>
+			</div>
+			<div class="shipping-place">
+				<span>Shipping in japan</span>
+			</div>
+		</div >
+	
+	</div > `
+
+		document.getElementById('data_container').innerHTML = dataValue
+
+	})
+}
+
+
+getData()
